1e011afb69f920bef5ee0e4049b42c47351c7c8a
Matt Pharoah <Matt.Pharoah@d2l.com>
2019-03-13 10:44:45 -0400

    Implemented ResizeObserver polyfill

--- d2l-resize-observer.js
+++ d2l-resize-observer.js
@@ -2,7 +2,38 @@ import ShadowMutationObserver from './shadow-mutation-observer.js';
 
 let _shadyObserver = null;
 let _shadowObserver = null;
+let _safariWorkaroundActive = false;
 const _watchedNodes = new Map();
+
+const _isSafari =
+	window.navigator.userAgent.indexOf( 'Safari/' ) >= 0 &&
+	window.navigator.userAgent.indexOf( 'Chrome/' ) === -1;
+	
+/* Safari's MutationObserver does not detect resizes on native textareas
+ * that occur as a result of the user dragging the resizer, so we just
+ * have to poll for changes in this case, but only on frames where the
+ * user could be resizing the textbox. Putting a mousemove event
+ * listener on this element won't work because the textbox lags behind
+ * the cursor, but we can at least only do a resize check when the mouse
+ * is moving instead of on every frame.
+ *
+ * This workaround is only used if there is a textarea element somewhere
+ * on the page that does not have 'resize: none' in its styling, and only
+ * if the browser is Safari.
+ */
+const updateSafariWorkaroundStatus = function( hasResizableTextArea ) {
+	hasResizableTextArea = !!hasResizableTextArea;
+	if( !_isSafari || _safariWorkaroundActive === hasResizableTextArea ) return;
+	
+	_safariWorkaroundActive = hasResizableTextArea;
+	if( hasResizableTextArea ) {
+		window.addEventListener( 'mousemove', onPossibleResize );
+		window.addEventListener( 'touchmove', onPossibleResize );
+	} else {
+		window.removeEventListener( 'mousemove', onPossibleResize );
+		window.removeEventListener( 'touchmove', onPossibleResize );
+	}
+};
 
 const destroy = function() {
 	if( _shadyObserver ) {
@@ -15,11 +46,14 @@ const destroy = function() {
 		_shadowObserver = null;
 	}
 	
+	updateSafariWorkaroundStatus( false );
 	window.removeEventListener( 'resize', onPossibleResize );
 	document.removeEventListener( 'transitionend', onPossibleResize );
+	_watchedNodes.clear();
 };
 
 const onPossibleResize = function() {
+	const observerMap = new Map();
 	_watchedNodes.forEach( ( nodeInfo, node ) => {
 		const newSize = node.getBoundingClientRect();
 		const lastSize = nodeInfo.lastSize;
@@ -32,20 +66,22 @@ const onPossibleResize = function() {
 		}
 		
 		nodeInfo.observers.forEach( observer => {
-			if( sizeChanged || ( posnChanged && observer.positionAware ) ) {
-				observer.callback( node, lastSize, newSize );
+			if( sizeChanged || observer.__positionAware !== false ) {
+				const resizeEntry = new ResizeObserverEntryPolyfill();
+				resizeEntry.__target = node;
+				resizeEntry.__contentRect = newSize;
+				
+				if( observerMap.has( observer ) ) {
+					observerMap.get( observer ).push( resizeEntry );
+				} else {
+					observerMap.set( observer, [resizeEntry] );
+				}
 			}
 		});
 	});
-};
-
-const onHasTextAreaChanged = function( hasTextArea ) {
-	_watchedNodes.forEach( nodeInfo => {
-		nodeInfo.observers.forEach( observer => {
-			if( observer.hasTextAreaCallback ) {
-				observer.hasTextAreaCallback( hasTextArea );
-			}
-		});
+	
+	observerMap.forEach( ( resizeEntries, observer ) => {
+		observer.__callback( resizeEntries );
 	});
 };
 
@@ -81,7 +117,7 @@ const initShadowObserver = function() {
 	}
 	
 	_shadowObserver = new ShadowMutationObserver( document.documentElement, onPossibleResize );
-	_shadowObserver.onHasTextareaChanged = onHasTextAreaChanged;
+	_shadowObserver.onHasTextareaChanged = updateSafariWorkaroundStatus;
 	_shadowObserver.onTransitionEnd = onPossibleResize;
 	
 	initCommon();
@@ -122,42 +158,58 @@ const removeListener = function( node, observer ) {
 	}
 };
 
-// TODO: maybe make this into a proper polyfill of ResizeObserver in next version?
-class D2LResizeObserver {
+class ResizeObserverEntryPolyfill {
+	get contentRect() { return this.__contentRect; }
+	get target() { return this.__target; }
+}
+
+class ResizeObserverPolyfill {
 	
-	constructor( callback, positionAware, hasTextAreaCallback ) {
-		this.callback = callback;
-		this.hasTextAreaCallback = hasTextAreaCallback;
-		this.positionAware = !!positionAware;
-		this._watchedElements = new Set();
+	constructor( callback ) {
+		this.__callback = callback;
+		this.__watchedElements = new Set();
 	}
 	
 	observe( node ) {
-		if( this._watchedElements.has( node ) ) {
+		if( this.__watchedElements.has( node ) ) {
 			return;
 		}
 		
 		lazyInit();
-		this._watchedElements.add( node );
+		this.__watchedElements.add( node );
 		addListener( node, this );
-		const size = node.getBoundingClientRect();
-		this.callback( node, size, size );
+		const resizeEntry = new ResizeObserverEntryPolyfill();
+		resizeEntry.__target = node;
+		resizeEntry.__contentRect = node.getBoundingClientRect();
+		this.__callback( [resizeEntry] );
 	}
 	
 	unobserve( node ) {
-		if( !this._watchedElements.has( node ) ) {
+		if( !this.__watchedElements.has( node ) ) {
 			return;
 		}
 		
 		removeListener( node, this );
-		this._watchedElements.delete( node );
+		this.__watchedElements.delete( node );
 	}
 	
 	disconnect() {
-		this._watchedElements.forEach( node => removeListener( node, this ) );
-		this._watchedElements.clear();
+		this.__watchedElements.forEach( node => removeListener( node, this ) );
+		this.__watchedElements.clear();
+	}
+}
+
+class ExtendedResizeObserver extends ResizeObserverPolyfill {
+	
+	constructor( callback, positionAware ) {
+		super( callback );
+		this.__positionAware = !!positionAware;
 	}
 	
 }
 
-export default D2LResizeObserver;
+export {
+	ResizeObserverPolyfill,
+	ExtendedResizeObserver,
+	ResizeObserverEntryPolyfill
+};
